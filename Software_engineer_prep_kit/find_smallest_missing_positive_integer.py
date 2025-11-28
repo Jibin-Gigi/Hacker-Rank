a253bd2c806c3022f3917067f56de2463f020547
Jibin Gigi <144718331+Jibin-Gigi@users.noreply.github.com>
2025-11-28 21:47:58 +0530

    findSmallestMissingPositive function
    Implement function to find the smallest missing positive integer from a list.

---
+++ Software_engineer_prep_kit/find_smallest_missing_positive_integer.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+#
+# Complete the 'findSmallestMissingPositive' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY orderNumbers as parameter.
+#
+
+def findSmallestMissingPositive(orderNumbers):
+    nums = set(orderNumbers)
+    smallest = 1
+    while smallest in nums:
+        smallest += 1
+    return smallest
+
+if __name__ == '__main__':
+    orderNumbers_count = int(input().strip())
+
+    orderNumbers = []
+
+    for _ in range(orderNumbers_count):
+        orderNumbers_item = int(input().strip())
+        orderNumbers.append(orderNumbers_item)
+
+    result = findSmallestMissingPositive(orderNumbers)
+
+    print(result)
