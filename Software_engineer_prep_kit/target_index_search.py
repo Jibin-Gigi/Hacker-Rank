828a572fed04016f16c9e093d56e319adc17e345
Jibin Gigi <144718331+Jibin-Gigi@users.noreply.github.com>
2025-11-29 23:50:20 +0530

    Add binarySearch function to locate target in array
    Implement binary search function to find target index.

---
+++ Software_engineer_prep_kit/target_index_search.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+#
+# Complete the 'binarySearch' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY nums
+#  2. INTEGER target
+#
+
+def binarySearch(nums, target):
+    if target in nums:
+        return nums.index(target)
+    return -1
+
+if __name__ == '__main__':
+    nums_count = int(input().strip())
+
+    nums = []
+
+    for _ in range(nums_count):
+        nums_item = int(input().strip())
+        nums.append(nums_item)
+
+    target = int(input().strip())
+
+    result = binarySearch(nums, target)
+
+    print(result)
