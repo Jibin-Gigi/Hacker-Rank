6ad3aea8d9ed68855f57836e5f012ecfc9b071c5
Jibin Gigi <144718331+Jibin-Gigi@users.noreply.github.com>
2025-12-02 19:11:07 +0530

    Add isAnagram function to validate anagrams
    Implement isAnagram function to check if two strings are anagrams.

---
+++ Software_engineer_prep_kit/check_valid_anagram.py
@@ -0,0 +1,49 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+#
+# Complete the 'isAnagram' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. STRING s
+#  2. STRING t
+#
+
+def isAnagram(s, t):
+    # if len(s) == len(t):
+    #     return 1 if sorted(s) == sorted(t) else 0
+    # return 0
+    
+    if len(s) != len(t):
+        return 0
+        
+    count = {}
+    for ch in s:
+        count[ch] = count.get(ch, 0) + 1
+    
+    for ch in t:
+        if ch in count:
+            count[ch] -= 1
+            if count[ch] < 0:
+                return 0
+        else: 
+            return 0
+            
+    return 1
+
+if __name__ == '__main__':
+    s = input()
+
+    t = input()
+
+    result = isAnagram(s, t)
+
+    print(result)
